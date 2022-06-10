1c2e80f6fdee15f7fe889cccacd3efcb97d6d43e
Itscailor <l.cairo@itsrizzoli.com>
2022-06-10 12:20:09 +0200

    fix: add input

--- design-system/react/src/atoms/button/Button.jsx
+++ design-system/react/src/atoms/button/Button.jsx
@@ -5,25 +5,7 @@ import PropTypes from 'prop-types';
  * Primary UI component for user interaction
  */
 
-export const input = ({placeholder, onData, isValid, type})=>{
-  function onInput(InputEvent){
-    console.log("input.onInput", InputEvent);
-    if(typeof onData == "function"){
-      onData(InputEvent.target.value);
-    }
-  }
-  const message = isValid ? "Valore buono" : "Valore non buono"
-  return(
-    <div><input
-    placeholder={placeholder}
-    onInput={onInput}
-    type={type}
-    >
-    </input>
-    {message}
-    </div>
-  );
-}
+
 
 export const Button = ({ label, onClick,type }) => {
   return (
