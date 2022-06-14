145e55ed08ab240c390605669965039e46df3304
Itscailor <l.cairo@itsrizzoli.com>
2022-06-14 12:18:27 +0200

    fix: add test and test stories

---
+++ design-system/react/src/atoms/text/Text.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export const Text = ({}) => {
+    return(
+        <span>ciao mondo</span>
+    )
+};
+Text.PropTypes = {
+
+};
+Text.defaultProps ={
+
+};
---
+++ design-system/react/stories/Text.stories.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import {Text} from '../src/atoms/text/Text';
+
+export default{
+    title: 'Example/Atoms/Text',
+    component: Text,
+    argTypes:{
+
+    },
+
+};
+const Template = (args) => <Text {...args} />;
+export const PlainText = Template.bind({});
+PlainText.args = {
+
+};
